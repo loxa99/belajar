baddab79dfe4ae0157a63772893602037f13b3e7
Kevin <michaeladreil5@gmail.com>
2021-10-15 17:43:45 +0700

    belajar route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return "<h1>Ini Home!</h1>"; });
+Route::get('/about', function () {
+    echo "<h1 style='text-align: center'><u>Tentang saya</u></h1>"; echo "<p>Ini website aku. <i>Never stop learning</i>. <strong>Jangan lupa tidur</strong>.</p>"; });
+
+Route::get('/salam/{nama}', function ($nama) {
+    echo "<h2>Good day, $nama!</h2>"; });
+
+Route::get('/produk/{nama?}/{qty?}', function ($nama = "N/A", $qty = 0) {
+     echo "<p>Jual <strong>$nama</strong>. Stok saat ini: $qty</p>"; });
+
+Route::fallback(function () { return "Wah kamu nyasar, turn back!"; });
